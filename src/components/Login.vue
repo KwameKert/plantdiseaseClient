54e0d5ac8f6b85ff47d391fdc07164ad29388bfb
kwamkert <tomriddle477@gmail.com>
2020-06-04 23:37:51 +0000

    login page udpdated

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,15 +3,16 @@
     <div class="row mt-4">
         <div class="col-md-4 offset-md-4">
               <div class="card">
-                <div class="card-body">
-                    <form >
+                <div class="card-body text-center">
+                    <h4>Login</h4>
+                    <form @submit.prevent="submitForm(form)">
                         <div class="form-group">
-                            <input type="text" class="form-control" placeholder="username">
+                            <input type="text" class="form-control" placeholder="username" v-model="form.username" >
                         </div>
                         <div class="form-group">
-                            <input type="password" class="form-control" placeholder="password">
+                            <input type="password" class="form-control" placeholder="password" v-model="form.password">
                         </div>
-                        <button class="btn btn-primary btn-block">Login</button>
+                        <button class="btn btn-primary btn-block" type="submit">Login</button>
                     </form>
                 </div>
         </div>
@@ -25,8 +26,20 @@
 <script>
 export default {
     name:'Login',
-    methods: {
-
+    data(){
+        return {
+            form:{
+                username:'',
+                password: ''
+            }
+            
+        }
+    },
+    methods: {  
+        submitForm: function(form){
+            let loginForm = JSON.parse(JSON.stringify(form))
+            console.log(loginForm)
+        }
     }
 }
 </script>
