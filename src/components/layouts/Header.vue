70eedd12df1911c31cf86ab729c15420f16664f0
kwamkert <tomriddle477@gmail.com>
2020-06-15 22:55:35 +0000

    header updated

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
        <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-        <button class="btn btn-primary" id="menu-toggle" @click="toggleMenu">Toggle Menu</button>
+        
+
+    <b-button size="sm" variant="outline" >
+      <b-icon icon="list" @click="toggleMenu"></b-icon>
+    </b-button>
 
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" >
           <span class="navbar-toggler-icon"></span>
@@ -9,23 +13,13 @@
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <li class="nav-item ">
+              <a class="nav-link" href="#">Account <b-icon icon="person"></b-icon> </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
+            <li class="nav-item ">
+              <a class="nav-link" href="#">Logout <b-icon icon="box-arrow-in-right"></b-icon></a>
             </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown
-              </a>
-              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li>
+            
           </ul>
         </div>
       </nav>
