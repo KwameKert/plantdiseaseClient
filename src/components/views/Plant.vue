f2cb939e111845e4e8cfa2f9adfc73cd2a9e320e
kwamkert <tomriddle477@gmail.com>
2020-06-17 00:48:24 +0000

    plant description added

--- src/components/views/Plant.vue
+++ src/components/views/Plant.vue
@@ -20,22 +20,24 @@
   <div class="row" v-if="response.plant">
 
       <div class=" col-sm-12 col-lg-12">
-        <b-card >
-            <div class="row justify-content-center">
-                <div class="col-md-4 col-sm-6 col-lg-4">
-                         <b-img thumbnail fluid :src="imageData" :alt="response.disaseName"></b-img>
-                    <img  alt="">
-                </div>
-
-                <div class="col-md-6 col-sm-6 col-lg-">
-                    <p><b>Plant:  {{response.plant}} </b></p>
-                    <p><b>Status:  {{response.status}} </b></p>
-                    <p><b>Remedies: {{response.remedy}} </b></p>
-                </div>
-            </div>
-          
-              
-        </b-card>
+          <div class="card">
+              <div class="card-header">
+                    <p><b>Plant : </b> {{response.plant}} </p>
+              </div>
+              <div class="card-body">
+                  <div class="row">
+                      <div class="col-4 text-center">
+                            <b-img thumbnail fluid :src="imageData" :alt="response.disaseName"></b-img>
+                            <p class="font-italic">{{response.status}}</p>
+                      </div>
+                      <div class="col-8">
+                            <h4>Description</h4>
+                            <p class="lead">{{response.remedy}}</p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      
       </div>
     </div>
 </div>
